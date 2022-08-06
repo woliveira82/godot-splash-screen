ad1eee09de52cc5007da6fbedd82e3b6752d1dad
Weiner Oliveira <woliveira82@gmail.com>
2022-08-06 22:30:14 +0100

    feat: setup splash screen and scene director

---
+++ src/control/SceneDirector.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+var scene_name: String
+
+
+func change_to(new_scene_name):
+	scene_name = new_scene_name
+	$AnimationPlayer.play("fade")
+
+
+func _new_scene():
+	get_tree().change_scene(scene_name)
---
+++ src/control/SplashScreen.gd
@@ -0,0 +1,27 @@
+extends ColorRect
+
+var splash_order: int = 0
+onready var images_list = $SplashList.get_children()
+
+
+func _ready():
+	$AnimationPlayer.play('close_curtain')
+
+
+func _to_main_menu():
+	SceneDirector.change_to('res://src/scenes/control/MainMenu.tscn')
+
+
+func _splash_image():
+	if splash_order >= len(images_list):
+		_to_main_menu()
+	
+	$AnimationPlayer.play('close_curtain')
+
+
+func _change_image():
+	for image in images_list:
+		image.visible = false
+	
+	images_list[splash_order].visible = true
+	splash_order += 1
