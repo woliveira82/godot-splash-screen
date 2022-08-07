050564b0adbe4b193e4c156d92c0dd86a143e573
Weiner Oliveira <woliveira82@gmail.com>
2022-08-07 14:45:10 +0100

    fix: pass mouse click through scene director

--- src/control/SceneDirector.gd
+++ src/control/SceneDirector.gd
@@ -3,6 +3,11 @@ extends CanvasLayer
 var scene_name: String
 
 
+func _ready():
+	$ColorRect.self_modulate.a = 0.0
+	$ColorRect.visible = false
+
+
 func change_to(new_scene_name):
 	scene_name = new_scene_name
 	$AnimationPlayer.play("fade")
